4776525fc438803ae57983e0bb1f9b91a0a1c5cf
svgnhkmglu <sevginhekimoglu@gmail.com>
2021-06-12 11:24:39 -0400

    JDBC connection

--- src/test/java/renastech/jdbc/day01/c1_DataBase_Connection.java
+++ src/test/java/renastech/jdbc/day01/c1_DataBase_Connection.java
@@ -1,10 +1,62 @@
 package renastech.jdbc.day01;
 
+import java.sql.*;
+
 public class c1_DataBase_Connection {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+
+        //JDBC has DriverManager class to manage connection according to
+        //  		the url, username , password
+        //
+        //  	url | connection string  (YOU WILL BE GIVE THIS INFORMATION)
+        //  	jdbc:oracle:thin:@54.88.118.39:1521:XE
+        //  	"jdbc:oracle:thin:@IP_ADDRESS:1521:XE";
+        //  	jdbc  --  connection using java
+        //  	oracle -- database vendor , RDBMS
+        //  	thin  --- one type oracle driver
+        //  	IP_ADDRESS  -- HOSTNAME or IP
+        //  	1521  ---- port used for oracle database
+        //  	XE   ----- sid name  unique identifier for your oracle db
+
+        String connectiobUrl="jdbc:oracle:thin:@54.88.118.39:1521:XE";
+
+        String userName="hr";
+        String password="hr";
+
+        //connection comes from library sql import java.sql.* ;
+        Connection connection= DriverManager.getConnection(connectiobUrl,userName,password);
+        //anytime you call sql methods,classes ,interfaces you also need to handle sql exception
 
 
+        //statment will be created through with connection that you have
+        Statement statement=connection.createStatement();
+
+
+        //third step is being able to pass and excute query
+        ResultSet resultSet=statement.executeQuery("SELECT * FROM countries");
+
+
+        //database cursor is on before each data
+        //to be able to move on inside countries table
+        //we need move the cursor
+
+        //cursor was on top table
+
+        resultSet.next();//now cursor is row one
+
+
+        //to be able to print data from table :
+        // you will need to use getString() method
+
+        //rs.getString(1) --->> return the first column cell value at this row
+        //		rs.getString("column name") --->> return the cell value at this column at this row
+
+
+        //there will be 2 way to print data from database first by passing index number of column
+        // or you need to pass column name .
+        System.out.println("First column value : " +resultSet.getString(1));
+        System.out.println("First column value : " +resultSet.getString("country_id"));
 
     }
 }
