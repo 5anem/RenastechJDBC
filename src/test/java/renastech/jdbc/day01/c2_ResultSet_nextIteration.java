5df2db3111e3a736129e6302d72ee3f69dda0a4f
svgnhkmglu <sevginhekimoglu@gmail.com>
2021-06-12 12:26:09 -0400

    resultset print whole table

--- src/test/java/renastech/jdbc/day01/c2_ResultSet_nextIteration.java
+++ src/test/java/renastech/jdbc/day01/c2_ResultSet_nextIteration.java
@@ -50,7 +50,29 @@ public class c2_ResultSet_nextIteration {
                     " , employee first name :" +resultSet.getString(2));
         }
 
-        resultSet.close();
+        resultSet.close(); // will only close your result set which was query  "select * from Employees"
+
+
+        //since i need to work on new query with same statment and same connection
+        //we dont need to close everythin
+        ResultSet resultSet2=statement.executeQuery("select * from Countries");
+
+        //print whole table of countries
+
+        //cursor is before table
+
+        while (resultSet2.next()){
+            System.out.println(resultSet2.getString("country_id") + " "
+            +resultSet2.getString("country_name") + " "
+            +resultSet2.getString("region_id"));
+        }
+
+
+        //
+        resultSet2.close();
+        statement.close();
+        connection.close();
+
 
 
 
