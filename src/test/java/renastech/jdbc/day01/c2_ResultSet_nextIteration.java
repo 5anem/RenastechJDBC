3e2d76b1fc8ec914db945322d1798376d53841f8
svgnhkmglu <sevginhekimoglu@gmail.com>
2021-06-12 12:03:37 -0400

    resultset next method

--- src/test/java/renastech/jdbc/day01/c2_ResultSet_nextIteration.java
+++ src/test/java/renastech/jdbc/day01/c2_ResultSet_nextIteration.java
@@ -28,5 +28,31 @@ public class c2_ResultSet_nextIteration {
 
         //print  second employee id and name from employees table
 
+        //cursor is on top of table
+        //to be able to get data you need to move your cursor
+
+        resultSet.next();//cursor is on first row which is first employee
+        resultSet.next();// cursor is on second
+
+        System.out.println("+++++++++++++++++++++++++++++++++++++++++");
+
+        System.out.println("employee id :" +resultSet.getString(1)   +
+                " , employee first name :" +resultSet.getString(2));
+        System.out.println("employee id :" +resultSet.getString("employee_id")   +
+                " , employee first name :" +resultSet.getString("first_name"));
+
+
+        System.out.println("+++++++++++++++++++++++++++++++++++++++++");
+
+        //this will continue to print from where cursor left
+        while (resultSet.next()){
+            System.out.println("employee id :" +resultSet.getString(1)   +
+                    " , employee first name :" +resultSet.getString(2));
+        }
+
+        resultSet.close();
+
+
+
     }
 }
